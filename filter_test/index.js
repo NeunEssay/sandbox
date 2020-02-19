573a27fe9185fc7be8e3a005959589904b0b30a2
MusAM <C227824632635e3>
2020-02-19 13:41:50 +0300

    filter_test

--- filter_test/index.js
+++ filter_test/index.js
@@ -1,10 +1,10 @@
 const FILTER_DIC = {
     "color": [],
-    "display": [],
-    "memory": [],
+    "display": [4.7],
+    "memory": [128],
     "model": [],
     "price": [],
-    "shops": []
+    "shops": ["трк_атриум"]
 }
 
 const IPHONE_DATA = 'data.json'
@@ -20,19 +20,38 @@ function filter_data(dataStructure, filterDic) {
     let filteredObj = []
 
     for (let i = 0; i < dataStructure.length; i++) {
+        let check_BOOL = false
 
-        for (let key in filterDic) {
-            if (filterDic[key].length !== 0) {
-                filterDic[key].forEach(function (filterItem) {
-                    dataStructure[i][key].forEach(function (dataItem) {
-                        if (filterItem != dataItem) {
-                            return
+        top : {
+            for (let key in filterDic) {
+                if (filterDic[key].length !== 0) {
+                    for (let j = 0; j < filterDic[key].length; j++) {
+                        if (Array.isArray(dataStructure[i][key]) === true) {
+                            for (let k = 0; k < dataStructure[i][key].length; k++) {
+                                if (dataStructure[i][key][k] == filterDic[key][j]) {
+                                    check_BOOL = true
+                                } else {
+                                    break top
+                                }
+                            }
                         } else {
-                            filteredObj.push(dataStructure[i])
+                            if (dataStructure[i][key] == filterDic[key][j]) {
+                                check_BOOL = true
+                            } else {
+                                break top
+                            }
                         }
-                    })
-                })
-            } else {
+                    }
+                } else if (filterDic[key].length === 0) {
+                    for (let j = 0; j < Object.keys(filterDic).length; j++) {
+                        if (filterDic[Object.keys(filterDic)[j]].length === 0 && (j + 1) === Object.keys(filterDic).length) {
+                            check_BOOL = true
+                        }
+                    }
+                }
+            }
+
+            if (check_BOOL === true) {
                 filteredObj.push(dataStructure[i])
             }
         }
