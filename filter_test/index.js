41577cda8e19f5ce961f764365e49bf89ce0c33d
MusAM <C227824632635e3>
2020-02-19 15:56:37 +0300

    filter_test

--- filter_test/index.js
+++ filter_test/index.js
@@ -1,10 +1,11 @@
 const FILTER_DIC = {
     "color": [],
-    "display": [4.7],
+    "display": [],
     "memory": [128],
     "model": [],
     "price": [],
-    "shops": ["трк_атриум"]
+    "shops": ["трк_авиапарк",
+        "трк_атриум"]
 }
 
 const IPHONE_DATA = 'data.json'
@@ -16,44 +17,56 @@ async function main(DATA) {
     console.log(filter_data(data, FILTER_DIC))
 }
 
+function valid_objects(check_BOOL, index, dataStructure, filterDic) {
+    for (let key in filterDic) {
+
+        if (filterDic[key].length !== 0) {
+
+            for (let j = 0; j < filterDic[key].length; j++) {
+
+                check_BOOL = false
+
+                if (Array.isArray(dataStructure[index][key]) === true) {
+                    for (let k = 0; k < dataStructure[index][key].length; k++) {
+                        if (dataStructure[index][key][k] == filterDic[key][j]) {
+                            check_BOOL = true
+                        } else {
+                            if (k + 1 === dataStructure[index][key][k]) {
+                                if (check_BOOL === false) {
+                                    return check_BOOL
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    if (dataStructure[index][key] == filterDic[key][j]) {
+                        check_BOOL = true
+                    } else {
+                        return check_BOOL
+                    }
+                }
+            }
+        } else if (filterDic[key].length === 0) {
+            for (let j = 0; j < Object.keys(filterDic).length; j++) {
+                if (filterDic[Object.keys(filterDic)[j]].length === 0) {
+                    if ((j + 1) === Object.keys(filterDic).length) {
+                        check_BOOL = true
+                    }
+                }
+            }
+        }
+    }
+    
+    return check_BOOL
+}
+
 function filter_data(dataStructure, filterDic) {
     let filteredObj = []
 
     for (let i = 0; i < dataStructure.length; i++) {
-        let check_BOOL = false
 
-        top : {
-            for (let key in filterDic) {
-                if (filterDic[key].length !== 0) {
-                    for (let j = 0; j < filterDic[key].length; j++) {
-                        if (Array.isArray(dataStructure[i][key]) === true) {
-                            for (let k = 0; k < dataStructure[i][key].length; k++) {
-                                if (dataStructure[i][key][k] == filterDic[key][j]) {
-                                    check_BOOL = true
-                                } else {
-                                    break top
-                                }
-                            }
-                        } else {
-                            if (dataStructure[i][key] == filterDic[key][j]) {
-                                check_BOOL = true
-                            } else {
-                                break top
-                            }
-                        }
-                    }
-                } else if (filterDic[key].length === 0) {
-                    for (let j = 0; j < Object.keys(filterDic).length; j++) {
-                        if (filterDic[Object.keys(filterDic)[j]].length === 0 && (j + 1) === Object.keys(filterDic).length) {
-                            check_BOOL = true
-                        }
-                    }
-                }
-            }
-
-            if (check_BOOL === true) {
-                filteredObj.push(dataStructure[i])
-            }
+        if (valid_objects(false, i, dataStructure, filterDic) === true) {
+            filteredObj.push(dataStructure[i])
         }
     }
 
