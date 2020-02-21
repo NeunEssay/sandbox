df477929eb69408de4ebac7aedb72c5941552247
MusAM <C227824632635e3>
2020-02-21 16:13:52 +0300

    filter_test

--- filter_test/filter.js
+++ filter_test/filter.js
@@ -1,47 +1,42 @@
-export function filter(data, item, dictionary) {
+function validate_item(item, dictionary) {
+    let check = true
 
-    function validate_item(item, dictionary) {
-        let check = true
+    for (let key in dictionary) {
 
-        for (let key in dictionary) {
+        for (let j = 0; j < dictionary[key].length; j++) {
 
-            for (let j = 0; j < dictionary[key].length; j++) {
+            check = false
 
-                check = false
-
-                if (Array.isArray(item[key]) === true) {
-                    for (let k = 0; k < item[key].length; k++) {
-                        if (item[key][k] == dictionary[key][j]) {
-                            check = true
-                            break
-                        }
-                    }
-                } else {
-                    if (item[key] == dictionary[key][j]) {
+            if (Array.isArray(item[key]) === true) {
+                for (let k = 0; k < item[key].length; k++) {
+                    if (item[key][k] == dictionary[key][j]) {
                         check = true
                         break
                     }
                 }
-            }
-            if (check == false) {
-                return check
-            }
-        }
-        return check
-    }
-
-    function filter_data(data, dictionary) {
-        let filteredData = []
-
-        for (let i = 0; i < data.length; i++) {
-
-            if (validate_item(data[i], dictionary) == true) {
-                filteredData.push(data[i])
+            } else {
+                if (item[key] == dictionary[key][j]) {
+                    check = true
+                    break
+                }
             }
         }
+        if (check == false) {
+            return check
+        }
+    }
+    return check
+}
 
-        return filteredData
+window['filter'] = function filter_data(data, dictionary) {
+    let filteredData = []
+
+    for (let i = 0; i < data.length; i++) {
+
+        if (validate_item(data[i], dictionary) == true) {
+            filteredData.push(data[i])
+        }
     }
 
-    return filter_data(data, dictionary)
+    return filteredData
 }
--- filter_test/main.js
+++ filter_test/main.js
@@ -1,5 +1,3 @@
-import {filter} from './filter.js'
-
 const IPHONE_DATA = 'data.json'
 
 const DICTIONARY = {
@@ -11,16 +9,10 @@ const DICTIONARY = {
     "shops": []
 }
 
-async function extract_data(DATA) {
-    const response = await fetch(DATA),
-        data = await response.json()
-
-    return data
-}
-
 function filter_options() {
-    extract_data(IPHONE_DATA)
-        .then(data => add_options(filter(data, data.forEach(item => item), DICTIONARY)))
+    fetch("data.json")
+        .then(response => response.json())
+        .then(data => add_options(filter(data, DICTIONARY)))
 }
 
 function add_to_dictionary(select, key) {
@@ -52,56 +44,46 @@ function add_select() {
             function () {
                 add_to_dictionary(select, key)
                 filter_options()
-                console.log(DICTIONARY)
             }
         )
     }
 }
 
-function check_for_equal(select, option) {
-    for (let i = 0; i < select.length; i++){
-        if (option == select.options[i].value) {
-            return true
-        } else if (select.length == i + 1) {
-            return false
+function unique_filter(arr, key) {
+    let selectArr = Array.prototype
+
+    arr.forEach(function (item) {
+        selectArr = selectArr.concat(item[key]).filter((value, index, selectArr) => selectArr.indexOf(value) == index)
+    })
+
+    return selectArr
+}
+
+function option_clean(select) {
+    let index = select.length
+
+    while (index--) {
+        if (select.options[index].value != select.options[select.selectedIndex].value && select.options[index].value != 'default') {
+            select.removeChild(select.options[index])
         }
     }
 }
 
 function add_options(filteredData) {
-    for (let key_1 in DICTIONARY) {
-        const select = document.getElementById('select-' + key_1)
 
-        for (let i = 0; i < select.length; i++) {
-            if (select.selectedIndex != i && select.options[i].value != 'default') {
-                select.removeChild(select.options[i])
-                i--
-            }
+    for (let key in DICTIONARY) {
+        const select = document.getElementById('select-' + key)
+
+        if (select.selectedIndex == 0) {
+            option_clean(select)
+            unique_filter(filteredData, key).forEach(value => {
+                let option = document.createElement('option')
+                option.textContent = value
+                select.appendChild(option)
+            })
+        } else {
+            option_clean(select)
         }
-
-        filteredData.forEach(function (item) {
-            for (let key_2 in item) {
-                if (key_1 == key_2) {
-                    if (select.selectedIndex == 0) {
-                        if (Array.isArray(item[key_2]) == false) {
-                            if (check_for_equal(select, item[key_2]) == false) {
-                                let option = document.createElement('option')
-                                option.textContent = item[key_2]
-                                select.appendChild(option)
-                            }
-                        } else {
-                            item[key_2].forEach(value => {
-                                if (check_for_equal(select, value) == false) {
-                                    let option = document.createElement('option')
-                                    option.textContent = value
-                                    select.appendChild(option)
-                                }
-                            })
-                        }
-                    }
-                }
-            }
-        })
     }
 }
 
