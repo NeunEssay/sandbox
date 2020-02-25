e9e356b49643e2b268c0de1d669c7a2fe24fc701
MusAM <C227824632635e3>
2020-02-25 16:08:28 +0300

    filter_test

--- filter_test/main.js
+++ filter_test/main.js
@@ -7,7 +7,7 @@ const DICTIONARY = {
     "shops": []
 }
 
-let DATA = []
+let DATA = Array.prototype
 
 function init() {
     fetch("data.json")
@@ -15,7 +15,7 @@ function init() {
         .then(data => data.forEach(item => DATA.push(item)))
         .then(function () {
             add_select()
-            add_options(DATA)
+            fill_options(DATA)
         })
 }
 
@@ -52,45 +52,70 @@ function add_select() {
     }
 }
 
-function unique_filter(arr, key) {
-    let selectArr = Array.prototype
+function unique_options_filter(arr, key) {
+    let selectArr = new Set()
 
-    arr.forEach(function (item) {
-        selectArr = selectArr.concat(item[key]).filter((value, index, selectArr) => selectArr.indexOf(value) == index)
+    arr.forEach(function(item) {
+        if (Array.isArray(item[key]) == true) {
+            item[key].forEach(value => selectArr.add(value))
+        } else {
+            selectArr.add(item[key])
+        }
     })
 
     return selectArr
 }
 
-function option_clean(select) {
-    let index = select.length
-
-    while (index--) {
-        if (select.options[index].value != select.options[select.selectedIndex].value && select.options[index].value != 'default') {
-            select.removeChild(select.options[index])
-        }
-    }
-}
-
-function add_options(data) {
-
+function fill_options(data) {
     for (let key in DICTIONARY) {
         const select = document.getElementById('select-' + key)
 
-        if (select.selectedIndex == 0) {
-            option_clean(select)
-            unique_filter(data, key).forEach(value => {
-                let option = document.createElement('option')
-                option.textContent = value
-                select.appendChild(option)
-            })
+        unique_options_filter(data, key).forEach(value => {
+            let option = document.createElement('option')
+            option.textContent = value
+            select.appendChild(option)
+        })
+    }
+}
+
+function option_disable(arr) {
+    for (let key in DICTIONARY) {
+        const select = document.getElementById('select-' + key)
+
+        let ableOptions = new Set
+
+        if (select.selectedIndex != 0) {
+            for (let i = 0; i < select.length; i++) {
+                if (select.options[i].value != 'default') {
+                    if (select.options[i].value != select.options[select.selectedIndex].value) {
+                        select.options[i].disabled = true
+                    }
+                }
+            }
         } else {
-            option_clean(select)
+
+            arr.forEach(function (item) {
+                if (Array.isArray(item[key]) == true) {
+                    item[key].forEach(value => ableOptions.add(value.toString()))
+                } else {
+                    ableOptions.add(item[key].toString())
+                }
+            })
+
+            for (let i = 0; i < select.length; i++) {
+                if (select.options[i].value != 'default') {
+                    if (ableOptions.has(select.options[i].value) == false) {
+                        select.options[i].disabled = true
+                    } else {
+                        select.options[i].disabled = false
+                    }
+                }
+            }
         }
     }
 }
 
 function filter_options(select, key) {
     add_to_dictionary(select, key)
-    add_options(filter_data(DATA, DICTIONARY))
+    option_disable(filter_data(DATA, DICTIONARY))
 }
